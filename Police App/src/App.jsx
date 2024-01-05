ec1ab063370bb1bddcbb801d3166347239e9532c
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-01-05 23:10:14 +0530

    Update

---
+++ Police App/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from'./components/Header'
+import Loginpage from './components/Loginpage'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+       <Header/>
+       <Loginpage/>
+       <div className="bg-gray-900 w-full h-screen"></div>
+    </div>
+  )
+}
+
+export default App
