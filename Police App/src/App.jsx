da0ebaaa6f218b4cc3398e2f496fcf3248aa1473
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-01-07 00:14:02 +0530

    Update App section

--- Police App/src/App.jsx
+++ Police App/src/App.jsx
@@ -1,17 +1,38 @@
-import './App.css'
-import Header from'./components/Header'
-import Loginpage from './components/Loginpage'
+import "./App.css";
+import Header from "./components/Header";
+import Loginpage from "./components/Loginpage";
+// import Complaint from "./components/Complaint";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RegisterForm from "./components/RegisterForm";
 
 function App() {
-  
-
   return (
-    <div className="App">
-       <Header/>
-       <Loginpage/>
-       <div className="bg-gray-900 w-full h-screen"></div>
+    <div className="App bg-gray-500">
+      <Router>
+        <Routes>
+          <Route path="/loginpage" element={<Loginpage />} />
+          {/* <Route path="/Complaint" element={<Complaint />} /> */}
+          <Route
+            path="/RegisterForm"
+            element={
+              <>
+                <Header />
+                <RegisterForm />
+              </>
+            }
+          />
+          <Route
+            path="/Complaint"
+            element={
+              <>
+                <Header />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
