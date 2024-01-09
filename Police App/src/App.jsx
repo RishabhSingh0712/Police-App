04cd24ce31aa4a4b11afbb4e1d471b3f0b5e39f5
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-01-09 22:44:10 +0530

    App routers

--- Police App/src/App.jsx
+++ Police App/src/App.jsx
@@ -4,23 +4,22 @@ import Loginpage from "./components/Loginpage";
 import Complaint from "./components/Complaint";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegisterForm from "./components/RegisterForm";
-// import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App bg-gray-500">
+    <div>
       <Router>
         <Routes>
-        
           <Route path="/loginpage" element={<Loginpage />} />
           <Route path="/complaint" element={<Complaint />} />
+          <Route path="/" element={<Header />} />
+
           <Route
             path="/RegisterForm"
             element={
               <>
-                {/* <Home/> */}
-                <Header />
-                <RegisterForm />
+              
+                <RegisterForm/>
               </>
             }
           />
