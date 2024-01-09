1307ca439fe6b3a2ed37dcc859165ba2dd496f63
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-01-09 22:45:11 +0530

    update login button

--- Police App/src/components/Loginpage.jsx
+++ Police App/src/components/Loginpage.jsx
@@ -76,7 +76,7 @@ const LoginPage = () => {
             </div>
             <button
               type="submit"
-              className="bg-yellow-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+              className="bg-yellow-500 hover:bg-blue-600 text-white px-4 py-2 rounded-full"
             >
               Login
             </button>
