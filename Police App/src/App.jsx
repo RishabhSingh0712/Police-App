a7edbb79456b77b8bd016139777193e72f43fa7a
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-01-11 23:02:18 +0530

    Update

--- Police App/src/App.jsx
+++ Police App/src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegisterForm from "./components/RegisterForm";
 import Service from "./components/Service";
 import Missing_Found from "./components/Missing_Found";
+import PersonForm from "./Form/PersonForm";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/complaint" element={<Complaint />} />
           <Route path="/service" element={<Service />} />
           <Route path="/missing_Found" element={<Missing_Found />} />
+          <Route path="/personform" element={<PersonForm />} />
           <Route path="/" element={<Header />} />
           <Route path="/RegisterForm"element={<> <RegisterForm/> </>} />
         </Routes>
